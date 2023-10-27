fd12f4f61acde3d263e83e2b31fa47f0a63e0ce7
Emmanuel <emmanuel.k.kangogo@gmail.com>
2023-10-27 21:14:16 +0300

    thorough revamp of the codes

--- extraction.py
+++ extraction.py
@@ -3,18 +3,38 @@ import numpy as np
 import re
 import subprocess
 
+def extract_region(row):
+    text = row["Region of Farming"]  # Assuming harvest season information is in the "Information" column
+    # Add regex pattern for extracting harvest season from the text
+    pattern = r'(rift valley|central|nairobi|eastern|north eastern|coast|western|nyanza)'  # Add more harvest seasons as needed
+    match = re.search(pattern, text, re.IGNORECASE)
+    if match:
+        return match.group(1).capitalize()  # Capitalize the harvest season for consistency
+    else:
+        return "Unknown Size"
+    
 def extract_crop_type(row):
-    text = row["Information"]  # Assuming crop type information is in the "Information" column
-    # Add regex pattern for extracting crop type from the text
-    pattern = r'(wheat|rice|corn|barley|potato|tomato|...)'  # Add more crop types as needed
+    text = str(row["Type of Crops"])  # Convert the input to a string
+    pattern = r'(maize|beans|vegetables|onions|potatoes|tomatoes)'  # Add more crop types as needed
     match = re.search(pattern, text, re.IGNORECASE)
     if match:
         return match.group(1).capitalize()  # Capitalize the crop type for consistency
     else:
         return "Unknown Crop"
 
+
+def extract_size_of_field(row):
+    text = str(row["Size of Farm"])  # Assuming harvest season information is in the "Information" column
+    # Add regex pattern for extracting harvest season from the text
+    pattern = r'(small|medium|large)'  # Add more harvest seasons as needed
+    match = re.search(pattern, text, re.IGNORECASE)
+    if match:
+        return match.group(1).capitalize()  # Capitalize the harvest season for consistency
+    else:
+        return "Unknown Size"
+
 def extract_harvest_season(row):
-    text = row["Information"]  # Assuming harvest season information is in the "Information" column
+    text = row["Harvest Season"]  # Assuming harvest season information is in the "Information" column
     # Add regex pattern for extracting harvest season from the text
     pattern = r'(spring|summer|fall|autumn|winter)'  # Add more harvest seasons as needed
     match = re.search(pattern, text, re.IGNORECASE)
@@ -24,31 +44,42 @@ def extract_harvest_season(row):
         return "Unknown Season"
 
 def extract_yield(row):
-    text = row["Information"]  # Assuming yield information is in the "Information" column
-    # Add regex pattern for extracting yield from the text
-    pattern = r'(\d+\.?\d*)\s*(?:tons|kg|pounds|bushels)'  # Add more units as needed
-    match = re.search(pattern, text, re.IGNORECASE)
-    if match:
-        return float(match.group(1))  # Convert yield to float for numerical value
+    value = row["Yield"]
+    if isinstance(value, (int, float)):
+        return value  # Return the numerical value as is
     else:
-        return np.nan  # Return NaN for missing yield information
+        text = str(value)  # Convert the input to a string
+        pattern = r'(\d+\.?\d*)'  # This pattern captures numerical values
+        match = re.search(pattern, text)
+        if match:
+            return float(match.group(1))  # Convert yield to float for numerical value
+        else:
+            return np.nan  # Return NaN for missing yield information
+
 
 def agriculture_produce_extraction():
-    data = pd.read_csv("/c/Users/emman/Documents/Github/ShambaTrend/agriculture_produce_data.csv")  # Replace with your agriculture produce data file path
+    """Runs the produce_predict python file"""
+    script_path = 'C:/Users/emman/Documents/Github/ShambaTrend/produce_predict.py'
+    subprocess.run(['python3', script_path])
 
-    # Add new columns for the extracted crop type, harvest season, and yield data.
-    data["Crop Type"] = data.apply(extract_crop_type, axis=1)
-    data["Harvest Season"] = data.apply(extract_harvest_season, axis=1)
-    data["Yield"] = data.apply(extract_yield, axis=1)
+data = pd.read_csv("C:/Users/emman/Documents/Github/ShambaTrend/Responses.csv")  # Replace with your agriculture produce data file path
 
-    # Remove null records in the Yield column.
-    data = data.dropna(subset=["Yield"])
+# Add new columns for the extracted crop type, harvest season, and yield data.
+    
+data["Region of Farming"] = data.apply(extract_region, axis=1)
+data["Type of Crops"] = data.apply(extract_crop_type, axis=1)
+data["Size of Farm"] = data.apply(extract_size_of_field, axis=1)
+data["Harvest Season"] = data.apply(extract_harvest_season, axis=1)
+data["Yield"] = data.apply(extract_yield, axis=1)
 
-    # Save the changes to a new CSV file without including unnecessary columns.
-    data = data.drop(["Information", "OtherColumnsToRemove"], axis=1)  # Add other unnecessary columns to remove
-    data.to_csv("/path/to/your/agriculture_produce_cleaned.csv", index=False)  # Replace with your desired output file path
+# Remove null records in the Yield column.
+data = data.dropna(subset=["Yield"])
 
-    # Call any additional functions or processes related to agriculture produce extraction here
-    # ...
+# Save the changes to a new CSV file without including unnecessary columns.
+data = data.drop(["Timestamp", "Name", "Phone", "2.1.", "2.2.", "3.1.", "3.2.", "4.2.", "4.1.", "5.1.", "5.2."], axis=1)
+data.to_csv("C:/Users/emman/Documents/Github/ShambaTrend/agriculture_produce_cleaned.csv", index=False)  # Replace with your desired output file path
+
+# Call any additional functions or processes related to agriculture produce extraction here
+# ...
 
 agriculture_produce_extraction()
--- produce_predict.py
+++ produce_predict.py
@@ -1,29 +1,55 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+import tensorflow as tf
+
+from tensorflow import keras
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
 
 # Load agriculture produce data (replace 'path/to/agriculture_produce_data.csv' with your file path)
-data = pd.read_csv("/c/Users/emman/Documents/Github/ShambaTrend/agriculture_produce_data.csv")
+data = pd.read_csv("C:/Users/emman/Documents/Github/ShambaTrend/agriculture_produce_cleaned.csv")
 
 # Map categorical variables to numerical values
-data["Crop Type"] = data["Crop Type"].map({
-    "Wheat": 1,
-    "Rice": 2,
-    "Corn": 3,
-    "Barley": 4,
-    "Potato": 5,
-    "Tomato": 6,
+
+data["Region of Farming"] = data["Region of Farming"].map({
+    "Rift Valley": 1,
+    "Central": 2,
+    "Nairobi": 3,
+    "Eastern": 4,
+    "North Eastern": 5,
+    "Coast": 6,
+    "Western": 7,
+    "Nyanza": 8
+    # Add more regions as needed
+})
+
+data["Type of Crops"] = data["Type of Crops"].map({
+    "Maize": 1,
+    "Beans": 2,
+    "Vegetables": 3,
+    "Onions": 4,
+    "Potatoes": 5,
+    "Tomatooes": 6
     # Add more crop types as needed
+})
+
+data["Size of Farm"] = data["Size of Farm"].map({
+    "Small": 1,
+    "Medium": 2,
+    "Large": 3
+    # Add more sizes as needed
 })
 
 data["Harvest Season"] = data["Harvest Season"].map({
     "Spring": 1,
     "Summer": 2,
     "Fall": 3,
-    "Autumn": 3,
-    "Winter": 4,
+    "Autumn": 5,
+    "Winter": 6
     # Add more harvest seasons as needed
 })
 
@@ -31,7 +57,7 @@ data["Harvest Season"] = data["Harvest Season"].map({
 data = data.dropna()
 
 # Prepare input features and target variable
-x = np.array(data[["Crop Type", "Harvest Season", "OtherFeatures"]])  # Add other features as needed
+x = np.array(data[["Region of Farming", "Type of Crops", "Size of Farm", "Harvest Season"]])  # Add other features as needed
 y = np.array(data[["Yield"]])
 
 # Split the data into training and testing sets
@@ -52,9 +78,13 @@ model.fit(xtrain, ytrain, batch_size=1, epochs=21)
 
 # Predict agriculture produce yield
 print("Enter Agriculture Produce Details to Predict Yield")
-a = int(input("Crop Type (Wheat = 1, Rice = 2, Corn = 3, Barley = 4, Potato = 5, Tomato = 6): "))
-b = int(input("Harvest Season (Spring = 1, Summer = 2, Fall = 3, Winter = 4): "))
+
+a = int(input("Region of Farming (Rift Valley= 1, Central= 2, Nairobi= 3, Eastern= 4, North Eastern= 5, Coast= 6, Western= 7, Nyanza= 8: "))
+b = int(input("Type of Crops (Maize = 1, Beans = 2, Vegetables = 3, Onions = 4, Potatoes = 5, Tomatoes = 6): "))
+c = int(input("Size of Farm (Small = 1, Medium = 2, Large = 3): "))
+d = int(input("Harvest Season (Spring = 1, Summer = 2, Fall = 3, Winter = 4): "))
+
 # Add input prompts for other features as needed
-features = np.array([[a, b, ...]])  # Add other features as needed
+features = np.array([[a, b, c, d ]])  # Add other features as needed
 predicted_yield = model.predict(features)
 print("Predicted Yield:", predicted_yield)
